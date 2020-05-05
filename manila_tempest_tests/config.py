696a057ca2db5558dfcfdf73bcbd4a0f34f2aeb2
Goutham Pacha Ravi <gouthampravi@gmail.com>
2020-05-05 08:41:07 -0700

    TrivialFix: Remove unnecessary import
    print_function was being imported, but
    not used in the module.
    
    Change-Id: Ie460cbe4779005e284af0652a14112c1939d4855
    Signed-off-by: Goutham Pacha Ravi <gouthampravi@gmail.com>

--- manila_tempest_tests/config.py
+++ manila_tempest_tests/config.py
@@ -13,8 +13,6 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from __future__ import print_function
-
 from oslo_config import cfg
 
 service_option = cfg.BoolOpt("manila",
